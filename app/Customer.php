115dec9860b96a269769a14a2dc1b03f921ae6d5
MaazKhan05 <maaz.khan@forrun.co>
2020-10-28 12:45:02 +0500

    mass assignment guarded

--- app/Customer.php
+++ app/Customer.php
@@ -7,6 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
+    /*fillable attributes are used to specify those fields which are to be mass assigned. Guarded attributes are used to specify those fields which are not mass assignable*/
+
+    //Fillable Example
+    //protected $fillable = ['name','email','phone_number','active'];
+
+   
+    //guarded Example
+    protected $guarded = [];
+
     public function scopeActive($query){
         return $query->where('active',1);
     }
--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -13,7 +13,7 @@ class CustomersController extends Controller
         // return view('customers',compact('customers'));
 
         $activeCustomers=Customer::active()->get();
-        $inActiveCustomers=Customer::in active()->get();
+        $inActiveCustomers=Customer::inactive()->get();
         return view('customers',compact('activeCustomers','inActiveCustomers'));
     }
 
@@ -24,17 +24,18 @@ class CustomersController extends Controller
         $data=request()->validate([
             'name'=>'required | min:3',
             'email'=>'required | email',
-            'phone_number'=>'required | numeric | min:11 ',
+            'phone_number'=>'required | numeric',
             'active'=>'required '
         ]);
-        
+
         //validation layer end
-        $customer = new Customer();
-        $customer->name=request('name');
-        $customer->email=request('email');
-        $customer->phone_number=request('phone_number');
-        $customer->active=request('active');
-        $customer->save();
+        Customer::create($data); //mass assgining data
+        // $customer = new Customer();
+        // $customer->name=request('name');
+        // $customer->email=request('email');
+        // $customer->phone_number=request('phone_number');
+        // $customer->active=request('active');
+        // $customer->save();
         return back();
     }
 }
